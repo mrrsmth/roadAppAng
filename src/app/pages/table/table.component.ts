d9c39eb49fbe6874b099492830cd8f44ac664760
alxs1dd93 <alxs1dd93@gmail.com>
2024-03-18 06:16:14 +0700

    httpclient, add table

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
-export class TableComponent {
 
+
+export class TableComponent {
+  roadmaps!: any[];
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.httpClient.get<any[]>('http://localhost:8082/roadmaps')
+      .subscribe(data => {
+        this.roadmaps = data;
+      });
+  }
 }
